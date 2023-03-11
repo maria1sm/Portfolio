7c59ae43a3973a86d58f88476a1e4e3c8b8c9af5
unknown <sjv.msismar97@gmail.com>
2023-03-11 04:05:47 +0100

    e2

---
+++ js/app.js
@@ -0,0 +1,44 @@
+//2
+
+function filter(c) {
+    let x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all-btn") {
+            c = "";
+    }
+    for (i = 0; i < x.length; i++) {
+      AddClass(x[i], "hide");
+      if (x[i].className.indexOf(c) > -1) RemoveClass(x[i], "hide");
+    }
+  }
+
+function AddClass(element, name) {
+let i, arr1, arr2;
+arr1 = element.className.split(" ");
+arr2 = name.split(" ");
+for (i = 0; i < arr2.length; i++) {
+if (arr1.indexOf(arr2[i]) == -1) {
+  element.className += " " + arr2[i];
+}
+}
+}      
+
+function RemoveClass(element, name) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+
+const btnContainer = document.querySelector('.boton__container');
+const btns = document.querySelectorAll('.boton');
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      btns.forEach(b => b.classList.toggle('color', b === btn));
+    });
+  });
